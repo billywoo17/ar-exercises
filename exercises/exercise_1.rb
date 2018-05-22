acc317deebf0f6e6f62bf447fe41a8604d0cf229
billywoo17 <billwu_@outlook.com>
2018-05-22 13:22:24 -0700

    finished exercise 4

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,6 +4,25 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
-store = Store.create {name:"Burnaby", annual_revenue:300000, mens_apparel:true, womens_apparel:true}
-storetwo = Store.create {name:"Richmond", annual_revenue:1260000, mens_apparel:false, womens_apparel:true}
-storethree = Store.create {name:"Gastown", annual_revenue:190000, mens_apparel:true, womens_apparel:false}
+storeone = Store.create
+storeone.name = "Burnaby"
+storeone.annual_revenue = 300000
+storeone.mens_apparel = true
+storeone.womens_apparel= true
+storeone.save
+
+storetwo = Store.create
+storetwo.name = "Richmond"
+storetwo.annual_revenue = 1260000
+storetwo.mens_apparel = false
+storetwo.womens_apparel= true
+storetwo.save
+
+storethree = Store.create
+storethree.name = "Gastown"
+storethree.annual_revenue = 190000
+storethree.mens_apparel = true
+storethree.womens_apparel= false
+storethree.save
+
+puts Store.count
