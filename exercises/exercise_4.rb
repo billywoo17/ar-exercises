99613643d8d0aee1cc60b381ae4bd51aaa027465
billywoo17 <billwu_@outlook.com>
2018-05-22 13:39:53 -0700

    fixed create

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,38 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
-@store4 = Store.create
-@store4.name = "Surrey"
-@store4.annual_revenue = 224000
-@store4.mens_apparel = false
-@store4.womens_apparel= true
-@store4.save
+@store4 = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+@store5 = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+@store6 = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
-@store5 = Store.create
-@store5.name = "Whistler"
-@store5.annual_revenue = 1900000
-@store5.mens_apparel = true
-@store5.womens_apparel= false
-@store5.save
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |stores|
+ puts "#{stores.name} #{stores.annual_revenue}"
+end
 
-@store6 = Store.create
-@store6.name = "Yaletown"
-@store6.annual_revenue = 430000
-@store6.mens_apparel = true
-@store6.womens_apparel= true
-@store6.save
-
- @mens_stores = Store.where(mens_apparel: true)
-
- @mens_stores.each do |stores|
+@womens_stores = Store.where(womens_apparel: true)
+@womens_stores.each do |stores|
+ if stores.annual_revenue < 1000000
    puts "#{stores.name} #{stores.annual_revenue}"
  end
-
- @womens_stores = Store.where(womens_apparel: true)
-
-
- @womens_stores.each do |stores|
-   if stores.annual_revenue < 1000000
-     puts "#{stores.name} #{stores.annual_revenue}"
-   end
- end
+end
