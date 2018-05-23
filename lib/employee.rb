924a020836e3641d8fd26021f2ab707e9b364f41
billywoo17 <billwu_@outlook.com>
2018-05-22 18:55:48 -0700

    finished exercise 8

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,14 @@
 class Employee < ActiveRecord::Base
-  # after_create:
+  after_create :generate_random_string
   belongs_to :store
   validates :first_name, :last_name, presence: true
   validates :hourly_rate, numericality: {less_than: 200, greater_than:40}
   validates_associated :store
+
+  private
+  def generate_random_string
+    @password = (0...6).map { ('a'..'z').to_a[rand(26)] }.join
+    self.password = @password
+    self.save!
+  end
 end
